0c2e9fdfe5bd5027a29893dc2c1f26fe1aab8b66
Tom Dyer <tdyer1@gmail.com>
2015-02-26 16:04:20 -0500

    Added Movie create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,18 @@ class MoviesController < ApplicationController
     render json: @movie
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      render json: @movie, status: :created, location: @movie
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :total_gross, :released_on)
+  end
 end
