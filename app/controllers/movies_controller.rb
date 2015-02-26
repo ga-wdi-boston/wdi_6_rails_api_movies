089af1cfdc83f68d3c546972d1e5082af37fea60
Tom Dyer <tdyer1@gmail.com>
2015-02-26 15:52:54 -0500

    Added show action for movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,10 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     render json: @movies
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render json: @movie
+  end
+
 end
