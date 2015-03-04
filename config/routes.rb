09926a064e484429d56d9272fa5a1f8d76c796f5
Tom Dyer <tdyer1@gmail.com>
2015-03-03 23:06:48 -0500

    removed unused review routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'movies#index'
 
-  resources :reviews, except: [:new, :edit]
+  # resources :reviews, except: [:new, :edit]
   resources :movies, except: [:new, :edit]
 end
